4cba140da4930b0fcd2be2daad3cd02cd45429d5
HJ <knowhrishi@gmail.com>
2023-05-30 22:33:51 +0200

    path updated

--- myapp/views.py
+++ myapp/views.py
@@ -14,7 +14,7 @@ from langchain.chains.llm import LLMChain
 from django.urls import reverse
 import urllib.parse
 
-cred = credentials.Certificate("/Users/knowhrishi/Documents/Code/Hackathons/HackBay23/resume_res/imployz-f8ee4-firebase-adminsdk-ei5aq-d9d56ea9fc.json")
+cred = credentials.Certificate("resume_res/imployz-f8ee4-firebase-adminsdk-ei5aq-d9d56ea9fc.json")
 firebase_admin.initialize_app(cred)
 
 db = firestore.client()
